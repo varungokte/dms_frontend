e59e83b1fc72ec9f3ab075d61827f6b87583a3ce
Mann Jadwani <mannjadwani2004@gmail.com>
2024-07-18 13:17:12 +0530

    dashboard

--- dms_frontend/src/PrivateRoutes.tsx
+++ dms_frontend/src/PrivateRoutes.tsx
@@ -5,52 +5,53 @@ import { useEffect, useState } from "react";
 import VerificationPage from "./components/AuthPages/VerificationPage";
 import { UserStatusList } from "./../Constants";
 
-function PrivateRoutes(){
-  const [token, setToken] = useState<any>(null);  
-  const {getDecryptedToken} = useGlobalContext();
-  useEffect(()=>{
-    getDecryptedToken().then((resToken:any)=>{
+function PrivateRoutes() {
+  const [token, setToken] = useState<any>(null);
+  const { getDecryptedToken } = useGlobalContext();
+  useEffect(() => {
+    getDecryptedToken().then((resToken: any) => {
       //console.log(resToken)
-     // console.log("private routes token", resToken)
+      // console.log("private routes token", resToken)
       /* if (JSON.stringify(token)===JSON.stringify(resToken) && Object.keys(resToken).length>0)
         return;
-       */if (resToken)
+       */
+      if (resToken)
         setToken(resToken);
       else
         setToken("INVALID");
-    }).catch(()=>{
+    }).catch(() => {
       setToken("INVALID");
     })
-  },[]);
+  }, []);
 
-  if (token=="INVALID")
-    return <Navigate to="/login"/>
+  if (token == "INVALID")
+    return <Navigate to="/login" />
   else if (token)
     return <EmailVerification token={token} />
   else
     return <></>
 }
 
-function EmailVerification(props:any){
-  useEffect(()=>{
+function EmailVerification(props: any) {
+  useEffect(() => {
     //console.log("email verification props",props)
-  },[])
+  }, [])
   //return <MenuRouter />
   if (!props.token)
-    return <Navigate to="/login"/>
-  if (props.token["S"]==UserStatusList[2]){
+    return <Navigate to="/login" />
+  if (props.token["S"] == UserStatusList[2]) {
     return <MenuRouter />
   }
   else
     return (
-    <>
-      <Routes>
-        <Route path="/verify" element={<VerificationPage/>} />
-      </Routes>
-      <Navigate to="/verify"/>
-    </>
-      
-  )
+      <>
+        <Routes>
+          <Route path="/verify" element={<VerificationPage />} />
+        </Routes>
+        <Navigate to="/verify" />
+      </>
+
+    )
 }
 
 export default PrivateRoutes;
